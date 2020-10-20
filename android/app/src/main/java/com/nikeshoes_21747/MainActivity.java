5af5c873507e02a1dbcb26180b10f99a95ea896e
Crowdbotics <team@crowdbotics.com>
2020-10-20 13:39:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/nikeshoes_21747/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nikeshoes_21747;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "nikeshoes_21747";
+    }
+}
